6723a5506f54c08022c1358d3e2592422a2c86b3
Mahmood <mahmood@Mahmoods-MacBook-Pro.local>
2019-09-30 22:25:31 +0530

    removed xl import

--- application/views/items/manage.php
+++ application/views/items/manage.php
@@ -58,10 +58,7 @@ $(document).ready(function()
 </script>
 
 <div id="title_bar" class="btn-toolbar print_hide">
-    <button class='btn btn-info btn-sm pull-right modal-dlg' data-btn-submit='<?php echo $this->lang->line('common_submit') ?>' data-href='<?php echo site_url($controller_name."/excel_import"); ?>'
-            title='<?php echo $this->lang->line('items_import_items_excel'); ?>'>
-        <span class="glyphicon glyphicon-import">&nbsp</span><?php echo $this->lang->line('common_import_excel'); ?>
-    </button>
+   
 
     <button class='btn btn-info btn-sm pull-right modal-dlg' data-btn-new='<?php echo $this->lang->line('common_new') ?>' data-btn-submit='<?php echo $this->lang->line('common_submit') ?>' data-href='<?php echo site_url($controller_name."/view"); ?>'
             title='<?php echo $this->lang->line($controller_name . '_new'); ?>'>
--- application/views/people/manage.php
+++ application/views/people/manage.php
@@ -33,10 +33,7 @@ $(document).ready(function()
 	if ($controller_name == 'customers')
 	{
 	?>
-		<button class='btn btn-info btn-sm pull-right modal-dlg' data-btn-submit='<?php echo $this->lang->line('common_submit') ?>' data-href='<?php echo site_url($controller_name."/excel_import"); ?>'
-				title='<?php echo $this->lang->line('customers_import_items_excel'); ?>'>
-			<span class="glyphicon glyphicon-import">&nbsp</span><?php echo $this->lang->line('common_import_excel'); ?>
-		</button>
+		
 	<?php
 	}
 	?>
